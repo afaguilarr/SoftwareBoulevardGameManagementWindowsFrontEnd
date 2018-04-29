895357cace3e4db0f8d660e10ef5d80fe1ff4f3e
Andrés Felipe Aguilar Rendón <afaguilarr@unal.edu.co>
2018-04-29 14:24:57 -0500

    First approximation

--- SoftwareBoulevard/src/app/app.module.ts
+++ SoftwareBoulevard/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
+const routes: Routes = [
+  {'path':'', 'redirectTo':'/home', 'pathMatch':'full'},
+  {'path':'home', 'component':HomeComponent},
+  {'path':'home/User', 'component':UserComponent},
+  {'path':'home/company', 'component':CompanyComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
