aba31896735d41386db3197bc2808522bd185550
Cordoba-Js <jjsernaco@unal.edu.co>
2018-05-02 21:34:43 -0500

    para error de team member

--- SoftwareBoulevard/src/app/header/header.component.ts
+++ SoftwareBoulevard/src/app/header/header.component.ts
@@ -12,13 +12,21 @@ export class HeaderComponent implements OnInit {
   constructor(public service: GeneralServiceService, public router: Router) { }
 
   ngOnInit() {
+
+  }
+
+  homeSubmit() {
     if(this.service.user_type === "Game Administrator"){
+      this.router.navigate([''])
     }
     else if(this.service.user_type === "Project Manager"){
+      this.router.navigate([''])
     }
     else if(this.service.user_type === "Team Member"){
+      this.router.navigate([''])
     }
     else{
+      this.router.navigate(['\'\', \'component'])
     }
   }
 
